cd5870b9dc8a388ac8d9fb10918a6c7032e3813c
Kailey09 <kcoop1323@gmail.com>
2022-03-05 23:37:11 -0500

    added quiz questions and for loop

--- script.js
+++ script.js
@@ -1,65 +1,65 @@
-var quizContainer = document.getElementById("quiz");
-var resultsContainer = document.getElementById("results");
-var submitButton = document.getElementById("submit");
-var startButton = document.getElementById("start");
-var nextButton = document.getElementById("next");
-var previousButton = document.getElementById("previous");
+var question = document.getElementById("question");
+var quizContainer = document.getElementById("quiz-container");
+var scoreCard = document.getElementById("scorecard");
+var option0 = document.getElementById("option0");
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var next = document.querySelectorAll("next");
+var points = document.querySelector("score");
+var span = document.querySelector("span");
+var score = 0;
+var i = 0
+
+
+
 
 
 var myQuestions = [
     {
         question: "Which one is an primitive data type?",
-        answers: {
-            1: "String",
-            2: "Number",
-            3: "Null",
-            4: "All of the Above"
-        },
-        correctAnswer: "4"
+        option: [ "String", "Number", "Null", "All of the Above"],
+        answer: "All of the Above"
     },
+    
     {
         question: "A block of code designed to preform a particular task is called a... ",
-        answers: {
-            1: "Function",
-            2: "Array",
-            3: "Parameter",
-            4: "Varible"
-        },
-        correctAnswer: "1"
+        option: [ "Function", "Array", "Parameter", "Varible"],
+        answer: "Function"
     },
 
     {
         question: "Method that outputs a message to the web console is called a...",
-        answers: {
-            1: "Console.debug",
-            2: "Console.count",
-            3: "Console.log",
-            4: "Console.clear"
-        },
-        correctAnswer: "3"
+        option: [ "Console.debug", "Console.count", "Console.log", "Console.clear"],
+        answer: "Conole.log"
     },
 
     {
         question: "A symbol representing a quantity that assumes a range of values.",
-        answers: {
-            1:  "Object",
-            2: "Variable",
-            3: "Document",
-            4: "Property"
-        },
-        correctAnswer: "2"
+        option: [ "Object", "Variable", "Document", "Property"],
+        answer: "Variable"
     },
     
 
     {
         question: "A variable you declare outside of a function is called a what?",
-        answers: {
-            1: "Local Scope",
-            2: "Hoisting",
-            3: "Global Scope",
-            4: "Block Scope"
-        },
-        correctAnswer: "3"
+        option: [ "Local Scope", "Hoisting", "Global Scope", "Block Scope"],
+        answer: "Global Scope"
     }
-]
+];
 
+var displayQuestons = function() {
+    for( var a = 0; a < span.length; a++);{
+    } 
+
+    question.innerHTML = "Q." + (i+1) + " " + myQuestions[i].question;
+
+    option0.innerHTML = myQuestions[i].option[0];
+    option1.innerHTML = myQuestions[i].option[1];
+    option2.innerHTML = myQuestions[i].option[2];
+    option3.innerHTML = myQuestions[i].option[3];
+
+    stat.innerHTML = "Question" + (i+1) + " " + myQuestions.length
+};
+
+displayQuestons();

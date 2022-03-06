f5a51cd30fb5cf74e0413809d16caa70a6beeaf8
Kailey09 <kcoop1323@gmail.com>
2022-03-06 17:11:56 -0500

    added timer

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-var question = document.getElementById("question");
+var questions = document.getElementById("questions");
 var quizContainer = document.getElementById("quiz-container");
-var scoreCard = document.getElementById("scorecard");
+var scoreBoard = document.getElementById("scoreboard");
 var option0 = document.getElementById("option0");
 var option1 = document.getElementById("option1");
 var option2 = document.getElementById("option2");
 var option3 = document.getElementById("option3");
-var next = document.querySelectorAll("next");
+var options = [
+    option0,
+    option1,
+    option2,
+    option3
+]
+
+var timeLeftSpan = document.getElementById("time-left");
+
+var next = document.getElementById("next");
 var points = document.querySelector("score");
 var span = document.querySelector("span");
 var score = 0;
-var i = 0
 
 
+var currentQuestion = 0;
+var timeLeft = 75;
+var timerInterval;
 
 
 
@@ -31,7 +42,7 @@ var myQuestions = [
     {
         question: "Method that outputs a message to the web console is called a...",
         option: [ "Console.debug", "Console.count", "Console.log", "Console.clear"],
-        answer: "Conole.log"
+        answer: "Console.log"
     },
 
     {
@@ -46,20 +57,66 @@ var myQuestions = [
         option: [ "Local Scope", "Hoisting", "Global Scope", "Block Scope"],
         answer: "Global Scope"
     }
-];
+]
 
-var displayQuestons = function() {
-    for( var a = 0; a < span.length; a++);{
-    } 
+var startTimer = function() {
 
-    question.innerHTML = "Q." + (i+1) + " " + myQuestions[i].question;
+    timerInterval = setInterval(function () {
+        timeLeftSpan.textContent = timeLeft;
+        timeLeft--
+    }, 1000)
+}
 
-    option0.innerHTML = myQuestions[i].option[0];
-    option1.innerHTML = myQuestions[i].option[1];
-    option2.innerHTML = myQuestions[i].option[2];
-    option3.innerHTML = myQuestions[i].option[3];
+var displayQuestions = function() {
+    question.innerHTML = "Q." + (currentQuestion+1) + " " + myQuestions[currentQuestion].question;
+    
+    // option0.textContent = myQuestions[currentQuestion].option[0];
+    // option1.textContent = myQuestions[currentQuestion].option[1];
+    // option2.textContent = myQuestions[currentQuestion].option[2];
+    // option3.textContent = myQuestions[currentQuestion].option[3];
 
-    stat.innerHTML = "Question" + (i+1) + " " + myQuestions.length
+    for( var i = 0; i < options.length; i++) {
+        options[i].textContent = myQuestions[currentQuestion].option[i];
+    }; 
 };
 
-displayQuestons();
+var calculate = function(s) {
+    if (s.innerHTML === myQuestions[i].answer && score < question.length){
+        score = score + 1;
+    }
+
+
+}
+
+
+var nextQuestion = function() {
+    currentQuestion++;
+
+    if(currentQuestion < myQuestions.length) {
+        displayQuestions();
+    } else {
+        clearInterval(timerInterval)
+        endQuiz()
+    }
+
+};
+
+var localStorage = function () {
+
+
+}
+
+var endQuiz = function () {
+    questions.style.display = "none";
+    scoreBoard.style.display = "block";
+
+    score = timeLeft;
+    alert("Your score is " + score)
+}
+
+
+displayQuestions();
+startTimer();
+
+next.addEventListener("click", nextQuestion);
+
